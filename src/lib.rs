7a5635a91ded39804c6fd6198168ee74182115df
pibragimov <pibragimov@whoosh.bike>
2024-07-18 23:54:28 +0300

    feat: add pyi

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod core;
 
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
